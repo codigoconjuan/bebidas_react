74a5864fa8972d7f47cd7d8154159dcc22779246
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-13 12:07:22 -0600

    primeros pasos

---
+++ src/components/Formulario.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Formulario = () => {
+    return ( 
+        <h1>Formulario aqui</h1>
+     );
+}
+ 
+export default Formulario;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <header className="bg-alert">
+            <h1>Busca Recetas de Bebidas</h1>
+        </header>
+     );
+}
+ 
+export default Header;
