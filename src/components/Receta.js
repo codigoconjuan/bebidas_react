97692c0292879706547e4fe8433b28b76ae3bd26
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-13 13:23:42 -0600

    imprimiendo el resto de informacion de recetas

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -5,6 +5,17 @@ const Receta = ({receta}) => {
         <div className="col-md-4 mb-3">
             <div className="card">
                  <h2 className="card-header">{receta.strDrink}</h2>
+
+                 <img className="card-img-top" src={receta.strDrinkThumb} alt={`Imagen de ${receta.strDrink}`} />
+
+                 <div className="card-body">
+                     <button
+                        type="button"
+                        className="btn btn-block btn-primary"
+                     >
+                         Ver Receta
+                     </button>
+                 </div>
             </div>
         </div>
      );
